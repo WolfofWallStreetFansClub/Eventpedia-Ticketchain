22f238a2ddca670f96e4e0d71cf1ab21b538acf4
Bryan Qiang <qiangbryan@gmail.com>
2018-02-10 16:05:39 -0500

    tmp commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
-import { AppContainer, hot } from 'react-hot-loader';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { AppContainer, hot } from 'react-hot-loader';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './style.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
